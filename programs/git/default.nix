efa4447d4edb0f05e096ec980bcb9176b8d4044f
Léo DUBOIN <leo@duboin.com>
2022-11-06 19:09:22 +0100

    git: Add aliases

--- programs/git/default.nix
+++ programs/git/default.nix
@@ -4,12 +4,47 @@
   enable = true;
 
   aliases = {
-    st = "status";
-    ci = "commit";
-    co = "checkout";
-    sw = "switch";
-    b = "branch";
+    ui = "!lazygit";
+
+    # push
+    p = "push";
+    pf = "push -f";
+    pt = "push --follow-tags";
+    psu = "push --set-upstream";
+
+    # commit
+    c  = "commit";
+    ca = "commit --amend";
+    ce = "commit --allow-empty";
+    cf = "commit --fixup";
+
+    # rebase
+    rb = "rebase";
+    rbi = "rebase -i";
+    rba = "rebase --abort";
+    rbc = "rebase --continue";
+
+    # log
+    lo = "log --oneline";
+    lg = "log --pretty=format:\"%C(yellow)%h%C(reset) %C(black)•%C(reset) %<(50)%s %C(green)(%cr) [%an] %C(auto)%d%C(reset)\" --abbrev-commit -30";
     graph = "log --graph --pretty=oneline --all --decorate=full";
+    st = "status -s  --ignore-submodules=untracked";
+    info = "log -n 1"; # Show last commit info
+
+    # branches
+    co = "checkout";
+    cob = "checkout -b";
+    del = "branch -D";
+    br = "branch --format='%(HEAD) %(align:15,left)%(color:yellow)%(refname:short)%(color:reset)%(end) %(color:black)•%(color:reset) %(align:50,left)%(contents:subject)%(end) %(color:green)(%(committerdate:relative)) [%(authorname)]%(color:reset)' --sort=-committerdate";
+
+    # reset
+    res = "reset --hard";
+    undo = "reset HEAD~1";
+    nuke = "!git reset --hard && git clean -df";
+
+    # submodules
+    sb = "submodule";
+    sbu = "submodule update --init";
   };
 
   includes = [
@@ -29,17 +64,21 @@
   extraConfig = {
     color.ui = true;
 
-    core.editor = "${pkgs.neovim}/bin/nvim";
+    core.editor = "nvim";
 
     merge.tool = "vimdiff";
     mergetool.vimdiff.cmd = "${pkgs.neovim}/bin/nvim -d $LOCAL $REMOTE $MERGED";
 
-    pager.status = false;
-    pager.commit = false;
+    pager = {
+      status = false;
+      commit = false;
+    };
 
     push.default = "simple";
     pull = { ff = "only"; rebase = true; };
 
     init.defaultBranch = "master";
+
+    rebase.autosquash = true;
   };
 }
