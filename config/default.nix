39f87069bc393355380e183ff9b35612fed16a5d
Léo DUBOIN <leo@duboin.com>
2023-03-07 04:22:28 +0100

    feat(config): Remove wallpapers

--- config/default.nix
+++ config/default.nix
@@ -1,17 +1,9 @@
 rec {
 
-  colorschemes = import ./colorschemes.nix;
-  wallpapers = import "${config.nixpkgs}/config/wallpapers.nix";
-
   config = rec {
     machine = "desktop";
     username = "leo";
     home = "/home/${config.username}";
     nixpkgs = "${config.home}/.config/nixpkgs";
-
-    colorscheme_name = "catpuccin";
-    colorscheme = colorschemes.catpuccin;
-
-    wallpaper = wallpapers.anime.sangatsu;
   };
 }
