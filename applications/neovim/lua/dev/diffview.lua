ec665155cbf6fe4f656d2a62fe4cab2054e3f2d6
Léo DUBOIN <leo@duboin.com>
2023-03-31 17:53:20 +0200

    fix(nvim: diffview): Remove deprecated options

--- applications/neovim/lua/dev/diffview.lua
+++ applications/neovim/lua/dev/diffview.lua
@@ -30,12 +30,14 @@ require("diffview").setup({
 
   file_history_panel = {
     log_options = {   -- See ':h diffview-config-log_options'
-      single_file = {
-        diff_merges = "combined",
-      },
-      multi_file = {
-        diff_merges = "first-parent",
-      },
+        git = {
+          single_file = {
+            diff_merges = "combined",
+          },
+          multi_file = {
+            diff_merges = "first-parent",
+          },
+        },
     },
     win_config = {
       position = "bottom",
