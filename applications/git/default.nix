98632abca3447d1706426e93c5b1c8b3111a9d37
Léo DUBOIN <leo@duboin.com>
2023-04-07 22:33:14 +0200

    feat(git): Add new functions using FZF

--- applications/git/default.nix
+++ applications/git/default.nix
@@ -9,7 +9,7 @@
 
       # push
       p = "push";
-      pf = "push -f";
+      pf = "push --force-with-lease";
       pt = "push --follow-tags";
       psu = "push --set-upstream";
 
@@ -31,13 +31,16 @@
       lg = "log --pretty=format:\"%C(yellow)%h%C(reset) %C(black)•%C(reset) %<(50)%s %C(green)(%cr) [%an] %C(auto)%d%C(reset)\" --abbrev-commit -30";
       graph = "log --graph --pretty=oneline --all --decorate=full";
       st = "status -s  --ignore-submodules=untracked";
+      dc = "diff --cached";
       info = "log -n 1"; # Show last commit info
+      history = "!git log --oneline | fzf | cut -d ' ' -f '1'"; # Find hash of an older commit using fuzzy finding
 
       # branches
       co = "checkout";
       cob = "checkout -b";
       del = "branch -D";
       br = "branch --format='%(HEAD) %(align:15,left)%(color:yellow)%(refname:short)%(color:reset)%(end) %(color:black)•%(color:reset) %(align:50,left)%(contents:subject)%(end) %(color:green)(%(committerdate:relative)) [%(authorname)]%(color:reset)' --sort=-committerdate";
+      bra = "br -a";
 
       # reset
       res = "reset --hard";
@@ -47,6 +50,12 @@
       # submodules
       sb = "submodule";
       sbu = "submodule update --init";
+
+      # files
+      ls = "!git ls-tree -r --name-only HEAD";
+      search = "!git ls | fzf";
+      edit = "!$EDITOR $(git search)";
+      tree = "!git ls | tree --fromfile .";
     };
 
     delta.enable = true;
@@ -71,5 +80,10 @@
 
       rebase.autosquash = true;
     };
+  };
+
+  programs.fish.functions = {
+    gcf = "git commit $argv --fixup (git history | cut -d ' ' - -f 1)";
+    groot = "cd (git rev-parse --show-toplevel)";
   };
 }
