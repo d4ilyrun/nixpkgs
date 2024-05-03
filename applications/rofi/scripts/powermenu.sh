a8399e1c65591a898e2e0fed41e786be23bff171
Léo DUBOIN <leo@duboin.com>
2024-05-03 23:40:30 +0200

    fix(rofi: powermenu): invalid i3 session name in logout
    The script did not count none+i3 as being an i3 session

--- applications/rofi/scripts/powermenu.sh
+++ applications/rofi/scripts/powermenu.sh
@@ -73,7 +73,7 @@ run_cmd() {
 				openbox --exit
 			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
 				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+            elif [[ "$DESKTOP_SESSION" =~ (.*\+)?i3 ]]; then
 				i3-msg exit
 			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
 				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
